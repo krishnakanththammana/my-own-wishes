16f4530f922a41edb7665283dee6f2dad3997145
krishnakanththammana <krishnakanththammana@gmail.com>
2017-04-04 23:21:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/ping', function(req, res) {
   res.send('pong');
 })
 
+app.get('/ping', function(req, res) {
+  res.send('pong');
+})
+
 String.prototype.capitalizeFirstLetter = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
@@ -24,9 +28,9 @@ app.get('/wish/:username', function(req, res) {
           '* {box-sizing: border-box;margin: 0; padding: 0;font-family: "Macondo", cursive;}'+
           'html, body {height: 100%; width: 100%;}'+
           'button {background: transparent; color: white; border: 1px solid white; padding: 0px 20px;margin-left: 20px;}'+
-          '.custom-wishes {text-align: center;position: absolute; bottom: 20px; color: white;}'+
-          '.main {position: absolute;color: white;font-size: 2em;text-align: center;width: 90%; margin: 0 auto;}'+
-          'body {display: flex; flex-direction: column; justify-content: center; align-items: center; background-color: rgba(0,0,0,0.4)}'+
+          '.custom-wishes {text-align: center; color: white;}'+
+          '.main {color: white;font-size: 2em;text-align: center;width: 90%; margin: 0 auto;}'+
+          'body {display: -webkit-box; display: -ms-flexbox;display: flex; -webkit-box-orient: vertical; -webkit-box-direction: normal; -ms-flex-direction: column; flex-direction: column; -webkit-box-pack: justify; -ms-flex-pack: justify; justify-content: space-between; -webkit-box-align: center; -ms-flex-align: center; align-items: center; background-color: rgba(0,0,0,0.4);}'+
           'html {min-height: 650px; height: 100%;background-image:url("https://media.giphy.com/media/IjmMzurYulKEw/giphy.gif");background-size: cover;background-repeat: no-repeat;}'+
         '</style>'+
       '</head>'+
